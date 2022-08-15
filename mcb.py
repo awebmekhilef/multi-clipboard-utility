88907079042b45461835512e249fee4ffe1494fc
Aweb Mekhilef <awebmekhilef@gmail.com>
2022-08-16 00:00:38 +1000

    Save messages in app data dir

--- mcb.py
+++ mcb.py
@@ -7,19 +7,37 @@ import pyperclip
 import os
 import sys
 import json
+from pathlib import Path
 
-# TODO: Clean up file name references
-# TODO: Save file to APPDATA
+# Use appdirs package in the future
+def get_appdata_dir():
+    home = Path().home()
+
+    if sys.platform == 'win32':
+        home /= 'AppData/Roaming'
+    elif sys.platform == 'linux':
+        home /= '.local/share'
+    elif sys.platform == 'darwin':
+        home /= 'Library/Application Support'
+
+    return home / 'mcb'
+
 
 def main():
+    # Setup data directory
+    dir = get_appdata_dir()
+    filepath = dir / 'messages.json'
+
+    dir.mkdir(parents=True, exist_ok=True)
+
     # Prep the messages.json file
-    if not os.path.exists('./messages.json') or not os.path.getsize('./messages.json') > 0:
-        with open('./messages.json', 'w') as file:
+    if not os.path.exists(filepath) or not os.path.getsize(filepath) > 0:
+        with open(filepath, 'w') as file:
             file.write('{}')
 
     # Saving a new message from clipboard
     if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
-        with open('./messages.json', 'r+') as file:
+        with open(filepath, 'r+') as file:
             messages = json.load(file)
             messages[sys.argv[2]] = pyperclip.paste()
 
@@ -32,7 +50,7 @@ def main():
     elif len(sys.argv) == 2:
         keyword = sys.argv[1].lower()
 
-        with open('./messages.json', 'r') as file:
+        with open(filepath, 'r') as file:
             messages = json.load(file)
 
             if keyword in messages:
